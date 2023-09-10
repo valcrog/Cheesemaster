39b18f3753d76eb4f93b7f14de99fb64ee7037d7
Valentin Crogiez <crogiez.valentin@gmail.com>
2023-09-10 22:53:11 +0200

    Register mod items from moditems class

--- src/main/java/net/valcrog/cheesemaster/Cheesemaster.java
+++ src/main/java/net/valcrog/cheesemaster/Cheesemaster.java
@@ -1,10 +1,12 @@
 package net.valcrog.cheesemaster;
 
 import net.fabricmc.api.ModInitializer;
+import net.valcrog.cheesemaster.item.ModItems;
 
 public class Cheesemaster implements ModInitializer {
     public static final String MOD_ID = "cheesemaster";
     @Override
     public void onInitialize() {
+        ModItems.registerModItems();
     }
 }
