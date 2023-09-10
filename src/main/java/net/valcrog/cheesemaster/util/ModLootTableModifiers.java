02bfc9405479c349eaaad33861e3917b66d44829
Valentin Crogiez <crogiez.valentin@gmail.com>
2023-09-10 23:27:30 +0200

    Updated loot tables modifier function to new fabric api

--- src/main/java/net/valcrog/cheesemaster/util/ModLootTableModifiers.java
+++ src/main/java/net/valcrog/cheesemaster/util/ModLootTableModifiers.java
@@ -1,29 +1,27 @@
 package net.valcrog.cheesemaster.util;
 
-import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
-import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
+
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.minecraft.loot.LootPool;
 import net.valcrog.cheesemaster.item.ModItems;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
-import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
-import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 import net.minecraft.util.Identifier;
-import net.valcrog.cheesemaster.item.ModItems;
 
 public class ModLootTableModifiers {
     private static final Identifier COW_ID =
             new Identifier("minecraft", "entities/cow");
 
     public static void modifyLootTables() {
-        LootTableLoadingCallback.EVENT.register(((resourceManager, manager, id, supplier, setter) -> {
-            if(COW_ID.equals(id)) {
-                FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+        LootTableEvents.MODIFY.register(((resourceManager, lootManager, id, tableBuilder, source) -> {
+            if(source.isBuiltin() && COW_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
                         .conditionally(RandomChanceLootCondition.builder(0.33f))
-                        .with(ItemEntry.builder(ModItems.RENNET_BAG))
-                        .withFunction(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 2.0f)).build());
-                supplier.withPool(poolBuilder.build());
+                        .with(ItemEntry.builder(ModItems.RENNET_BAG));
+
+                tableBuilder.pool(poolBuilder);
             }
         }));
     }
