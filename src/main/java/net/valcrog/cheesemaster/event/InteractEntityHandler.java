685a663bc00fefd03fc9cd190872cfd1f54f85b9
Valentin Crogiez <crogiez.valentin@gmail.com>
2023-09-11 00:25:47 +0200

    Created custom interact entity handler

---
+++ src/main/java/net/valcrog/cheesemaster/event/InteractEntityHandler.java
@@ -0,0 +1,36 @@
+package net.valcrog.cheesemaster.event;
+
+import net.fabricmc.fabric.api.event.player.UseEntityCallback;
+import net.minecraft.client.resource.language.I18n;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityInteraction;
+import net.minecraft.entity.passive.CowEntity;
+import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.entity.passive.SheepEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsage;
+import net.minecraft.item.Items;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.world.World;
+import net.valcrog.cheesemaster.item.ModItems;
+import org.jetbrains.annotations.Nullable;
+import net.minecraft.text.Text;
+
+public class InteractEntityHandler implements UseEntityCallback {
+    public ActionResult interact(PlayerEntity player, World world, Hand hand, Entity entity, @Nullable EntityHitResult hitResult) {
+        if (entity instanceof SheepEntity && !world.isClient()) {
+            ItemStack itemStack = player.getStackInHand(hand);
+            if (itemStack.isOf(Items.BUCKET) && !((SheepEntity) entity).isBaby()) {
+                player.playSound(SoundEvents.ENTITY_COW_MILK, 1.0F, 1.0F);
+                ItemStack itemStack2 = ItemUsage.exchangeStack(itemStack, player, ModItems.SHEEPS_MILK_BUCKET.getDefaultStack());
+                player.setStackInHand(hand, itemStack2);
+            }
+        }
+        return ActionResult.PASS;
+    }
+
+}
