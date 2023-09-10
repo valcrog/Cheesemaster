28bc99556bf98d1d40a93c6ffeea53819b73dff9
Valentin Crogiez <crogiez.valentin@gmail.com>
2023-09-10 22:52:51 +0200

    Created client mod class

---
+++ src/main/java/net/valcrog/cheesemaster/CheesemasterClientMod.java
@@ -0,0 +1,10 @@
+package net.valcrog.cheesemaster;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CheesemasterClientMod implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
