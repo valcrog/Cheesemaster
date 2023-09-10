1e9d4ef01265c9fe8526beb1c28122a8bfe41995
Valentin Crogiez <crogiez.valentin@gmail.com>
2023-09-10 20:05:49 +0200

    Initial commit

---
+++ src/main/java/net/valcrog/cheesemaster/Cheesemaster.java
@@ -0,0 +1,13 @@
+package net.valcrog.cheesemaster;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Cheesemaster implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
