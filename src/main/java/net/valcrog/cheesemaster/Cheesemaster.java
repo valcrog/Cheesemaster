5b5c786bf5f19423c06f356a858c2476d9e526e5
Valentin Crogiez <crogiez.valentin@gmail.com>
2023-09-10 20:27:57 +0200

    Added mod id constant

--- src/main/java/net/valcrog/cheesemaster/Cheesemaster.java
+++ src/main/java/net/valcrog/cheesemaster/Cheesemaster.java
@@ -1,12 +1,14 @@
 package net.valcrog.cheesemaster;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
 
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 public class Cheesemaster implements ModInitializer {
     public static final Logger LOGGER = LogManager.getLogManager().getLogger("cheesemaster");
+    public static final String MOD_ID = "cheesemaster";
     @Override
     public void onInitialize() {
         LOGGER.info("Hello world!");
