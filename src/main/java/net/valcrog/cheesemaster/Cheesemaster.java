355eb67c04a805e67288c8b6b4c49892827d1e96
Valentin Crogiez <crogiez.valentin@gmail.com>
2023-09-10 20:10:03 +0200

    Fixed some direction error

--- src/main/java/net/valcrog/cheesemaster/Cheesemaster.java
+++ src/main/java/net/valcrog/cheesemaster/Cheesemaster.java
@@ -2,12 +2,13 @@ package net.valcrog.cheesemaster;
 
 import net.fabricmc.api.ModInitializer;
 
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
 public class Cheesemaster implements ModInitializer {
-    /**
-     * Runs the mod initializer.
-     */
+    public static final Logger LOGGER = LogManager.getLogManager().getLogger("cheesemaster");
     @Override
     public void onInitialize() {
-
+        LOGGER.info("Hello world!");
     }
 }
--- src/main/java/net/valcrog/cheesemaster/mixin/ExampleMixin.java
+++ src/main/java/net/valcrog/cheesemaster/mixin/ExampleMixin.java
@@ -1,15 +1,16 @@
 package net.valcrog.cheesemaster.mixin;
 
-import net.minecraft.server.MinecraftServer;
+import net.valcrog.cheesemaster.Cheesemaster;
+import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftServer.class)
+@Mixin(TitleScreen.class)
 public class ExampleMixin {
-    @Inject(at = @At("HEAD"), method = "loadWorld")
+    @Inject(at = @At("HEAD"), method = "init()V")
     private void init(CallbackInfo info) {
-        // This code is injected into the start of MinecraftServer.loadWorld()V
+        Cheesemaster.LOGGER.info("This line is printed by an example mod mixin!");
     }
 }
