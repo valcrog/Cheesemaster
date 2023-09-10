c567db7208a891eab84bec8ec92f78b1e4b6c2dd
Valentin Crogiez <crogiez.valentin@gmail.com>
2023-09-10 23:23:27 +0200

    Created ModLootTableModifiers class and added rennet bag to cow's drop table

--- src/main/java/net/valcrog/cheesemaster/Cheesemaster.java
+++ src/main/java/net/valcrog/cheesemaster/Cheesemaster.java
@@ -2,11 +2,13 @@ package net.valcrog.cheesemaster;
 
 import net.fabricmc.api.ModInitializer;
 import net.valcrog.cheesemaster.item.ModItems;
+import net.valcrog.cheesemaster.util.ModLootTableModifiers;
 
 public class Cheesemaster implements ModInitializer {
     public static final String MOD_ID = "cheesemaster";
     @Override
     public void onInitialize() {
         ModItems.registerModItems();
+        ModLootTableModifiers.modifyLootTables();
     }
 }
---
+++ src/main/java/net/valcrog/cheesemaster/util/ModLootTableModifiers.java
@@ -0,0 +1,30 @@
+package net.valcrog.cheesemaster.util;
+
+import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
+import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
+import net.valcrog.cheesemaster.item.ModItems;
+import net.minecraft.loot.condition.RandomChanceLootCondition;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.function.SetCountLootFunction;
+import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
+import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+import net.minecraft.util.Identifier;
+import net.valcrog.cheesemaster.item.ModItems;
+
+public class ModLootTableModifiers {
+    private static final Identifier COW_ID =
+            new Identifier("minecraft", "entities/cow");
+
+    public static void modifyLootTables() {
+        LootTableLoadingCallback.EVENT.register(((resourceManager, manager, id, supplier, setter) -> {
+            if(COW_ID.equals(id)) {
+                FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(0.33f))
+                        .with(ItemEntry.builder(ModItems.RENNET_BAG))
+                        .withFunction(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 2.0f)).build());
+                supplier.withPool(poolBuilder.build());
+            }
+        }));
+    }
+}
